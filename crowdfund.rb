94ee67e2c4928172290583379a5b385f4fd76dab
Adam89 <Adamm1@aston.ac.uk>
2013-11-21 21:05:30 -0700

    basic ruby app

---
+++ crowdfund.rb
@@ -0,0 +1,7 @@
+project_name1 = "ABC"
+project_name2 = "LMN"
+project_name3 = "XYZ"
+funds = $1000
+puts "Project #{project_name1} has #{funds} in funding"
+
+puts "Projects: \n\t#{project_name1}\n\t#{project_name2}\n\t#{project_name3}"
