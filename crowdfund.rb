1a0e806fecda951f960a94d02af647c770a01c06
Adam89 <Adamm1@aston.ac.uk>
2013-11-23 23:18:17 -0700

    created a basic class

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,11 +1,23 @@
-def time
-  Time.new.strftime("as of %d/%m/%y")
+class Crowdfund
+  def initialize(name, fund=0)
+    @name = name
+    @fund = fund
+  end
+
+  def time
+    Time.new.strftime("as of %d/%m/%y")
+  end
+
+  def to_s
+    "Project #{name.upcase} has $#{fund} in funds as of #{time}"
+  end
+
+  def donation
+    @fund += 100
+  end
 end
 
-def fundraising_project(name, fund=0)
-  "Project #{name.upcase} has $#{fund} in funds as of #{time}"
-end
-puts fundraising_project("cour", 1000)
-puts fundraising_project("build", 1500)
-puts fundraising_project("salf")
-
+first_project = Crowdfund.new('poor', 100)
+puts first_project
+first_project.donation
+puts first_project
