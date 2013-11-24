191057b97e6c3742a98cc3ebf10898dfeb38daac
Adam89 <Adamm1@aston.ac.uk>
2013-11-23 23:26:06 -0700

    added more methods to calss

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,23 +1,33 @@
 class Crowdfund
-  def initialize(name, fund=0)
+  def initialize(name, fund=0, goal=3000)
     @name = name
     @fund = fund
-  end
-
-  def time
-    Time.new.strftime("as of %d/%m/%y")
+    @goal = goal
   end
 
   def to_s
-    "Project #{name.upcase} has $#{fund} in funds as of #{time}"
+    "Project #{@name.upcase} has $#{@fund} in funding towards a goal of $#{@goal}"
   end
 
-  def donation
-    @fund += 100
+  def lost_funds
+    puts"Project #{@name.upcase} lost some funds!"
+    @fund -= 15
   end
+
+  def got_funds
+    puts "Project #{@name.upcase} got more funds!"
+    @fund += 25
+  end
+
+
 end
 
-first_project = Crowdfund.new('poor', 100)
+first_project = Crowdfund.new('xyz', 25, 75)
 puts first_project
-first_project.donation
+second_project = Crowdfund.new('lmn', 500)
+puts second_project
+second_project.lost_funds
+first_project.got_funds
+
+puts second_project
 puts first_project
